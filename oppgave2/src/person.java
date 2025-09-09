2698ffdf39af47b0a4d7db58bb706430e9761bc6
nesad2515 <lgo5368@gmail.com>
2025-09-09 19:08:01 +0200

    Add files via upload

--- oppgave2/src/person.java
+++ oppgave2/src/person.java
@@ -2,7 +2,11 @@
 public class person {
     public static void main(String [] args){
         personClass person2 = new personClass();
+        personClass person3 = new personClass();
+
+        person3.SetPersonClass();
         person2.SetPersonClass();
+
     }
 }
 
